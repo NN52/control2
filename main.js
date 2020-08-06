1dd0eb55c8fc513a87da728ccdb45cf5da46168f
NN52 <69294584+NN52@users.noreply.github.com>
2020-08-06 13:20:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
-Alert(" - 1 - ");  
+alert(" Основной ");
+var items = [{
+            city: 'Milano',
+            country: 'Italy',
+            link: 'details.html#42'
+        },
+        {
+            city: 'London',
+            country: 'England',
+            link: 'details.html#10'
+        },
+        {
+            city: 'Rome',
+            country: 'Italy',
+            link: 'details.html#39'
+        }
 
-Alert(" - 3 - ");   
+    ];
+
+    var container = document.getElementById("table").getElementsByTagName("tbody")[0];
+
+    items.forEach(item => {
+        var tr = document.createElement("tr");
+
+        var cityName = document.createElement('a');
+        cityName.href = item.link;
+        cityName.innerHTML = item.city;
+
+        var cityColumn = document.createElement('td');
+        cityColumn.appendChild(cityName);
+
+        var countryColumn = document.createElement('td');
+        countryColumn.innerHTML = item.country;
+
+        tr.appendChild(cityColumn);
+        tr.appendChild(countryColumn);
+
+        container.appendChild(tr);
+    });
