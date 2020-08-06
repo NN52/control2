cb36347d16862aa3102293b8140c683f962f391e
NN52 <69294584+NN52@users.noreply.github.com>
2020-08-06 15:34:11 +0300

    Update one.js

--- one.js
+++ one.js
@@ -1,1 +1,7 @@
 alert(" Один ");
+$.get('https://study.ps-gkh.ru/v8_nn_zhkh20_study/hs/api/v1/ping/', {query: 'test'}, function(data) { 
+    console.log(data); // ответ от сервера
+})
+.success(function() { console.log('Успешное выполнение'); })
+.error(function(jqXHR) { console.log('Ошибка выполнения'); })
+.complete(function() { console.log('Завершение выполнения'); });
