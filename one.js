3918e15a920702bb38b4706681833ad32882e788
NN52 <69294584+NN52@users.noreply.github.com>
2020-08-06 15:57:46 +0300

    Update one.js

--- one.js
+++ one.js
@@ -1,7 +1,10 @@
-alert(" Один ");
-$.get('https://study.ps-gkh.ru/v8_nn_zhkh20_study/hs/api/v1/ping/', {query: 'test'}, function(data) { 
-    console.log(data); // ответ от сервера
-})
-.success(function() { console.log('Успешное выполнение'); })
-.error(function(jqXHR) { console.log('Ошибка выполнения'); })
-.complete(function() { console.log('Завершение выполнения'); });
+alert(" 1 Один ");
+$.ajax({
+    type: 'GET',
+    url: 'https://study.ps-gkh.ru/v8_nn_zhkh20_study/hs/api/v1/ping/',
+    data: {query: 'test'},
+    dataType: 'text',
+    success: function(data) { console.log(data); }, // обработка ответа от сервера
+    error: function(jqXHR) { console.log('Ошибка выполнения'); },
+    complete: function() { console.log('Завершение выполнения'); }
+});
